667732841bea4461fc1bfac1589319e30658f604
Teddysun <i@teddysun.com>
2020-08-29 00:34:47 +0900

    Update build-release.sh
    Go 1.15 drops support for 32-bit binaries on macOS, iOS, iPadOS, watchOS, and tvOS (the darwin/386 and darwin/arm ports)
    Reference URL: https://tip.golang.org/doc/go1.15#darwin
    
    Signed-off-by: Teddysun <i@teddysun.com>

--- build-release.sh
+++ build-release.sh
@@ -3,17 +3,17 @@
 sum="sha1sum"
 
 if ! hash sha1sum 2>/dev/null; then
-	if ! hash shasum 2>/dev/null; then
-		echo "I can't see 'sha1sum' or 'shasum'"
-		echo "Please install one of them!"
-		exit
-	fi
-	sum="shasum"
+    if ! hash shasum 2>/dev/null; then
+        echo "I can't see 'sha1sum' or 'shasum'"
+        echo "Please install one of them!"
+        exit
+    fi
+    sum="shasum"
 fi
 
 [[ -z $upx ]] && upx="echo pending"
 if [[ $upx == "echo pending" ]] && hash upx 2>/dev/null; then
-	upx="upx -9"
+    upx="upx -9"
 fi
 
 VERSION=$(git describe --tags)
@@ -25,17 +25,21 @@ ARCHS=(amd64 386)
 mkdir bin
 
 for os in ${OSES[@]}; do
-	for arch in ${ARCHS[@]}; do
-		suffix=""
-		if [ "$os" == "windows" ]
-		then
-			suffix=".exe"
-		fi
-		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -v -ldflags "$LDFLAGS" -o v2ray-plugin_${os}_${arch}${suffix}
-		$upx v2ray-plugin_${os}_${arch}${suffix} >/dev/null
-		tar -zcf bin/v2ray-plugin-${os}-${arch}-$VERSION.tar.gz v2ray-plugin_${os}_${arch}${suffix}
-		$sum bin/v2ray-plugin-${os}-${arch}-$VERSION.tar.gz
-	done
+    for arch in ${ARCHS[@]}; do
+        # Go 1.15 drops support for 32-bit binaries on macOS, iOS, iPadOS, watchOS, and tvOS (the darwin/386 and darwin/arm ports)
+        # Reference URL: https://tip.golang.org/doc/go1.15#darwin
+        if [ "$os" == "darwin" ] && [ "$arch" == "386" ]; then
+            continue
+        fi
+        suffix=""
+        if [ "$os" == "windows" ]; then
+            suffix=".exe"
+        fi
+        env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -v -ldflags "$LDFLAGS" -o v2ray-plugin_${os}_${arch}${suffix}
+        $upx v2ray-plugin_${os}_${arch}${suffix} >/dev/null
+        tar -zcf bin/v2ray-plugin-${os}-${arch}-$VERSION.tar.gz v2ray-plugin_${os}_${arch}${suffix}
+        $sum bin/v2ray-plugin-${os}-${arch}-$VERSION.tar.gz
+    done
 done
 
 # ARM
