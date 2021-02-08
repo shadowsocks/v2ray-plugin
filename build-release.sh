3cd1e2e2bb0f9f4e8614199ff084c159a21b77c6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-07 22:20:02 -0500

    Update build-release.sh

--- build-release.sh
+++ build-release.sh
@@ -15,7 +15,7 @@ dist-check
 # Pre-Checks system requirements
 function installing-system-requirements() {
     if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
-        if [ ! -x "$(command -v sha1sum)" ]; then
+        if { [ ! -x "$(command -v sha1sum)" ] || [ ! -x "$(command -v shasum)" ]; }; then
             if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
                 sudo apt-get update && apt-get install coreutils -y
             elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
@@ -40,22 +40,17 @@ installing-system-requirements
 # Build for all the OS
 function build-golang-app() {
     if [ -x "$(command -v go)" ]; then
-        GOOS=aix GOARCH=ppc64 go build -o build/aix-ppc64 .
-        GOOS=android GOARCH=386 go build -o build/android-386 .
-        GOOS=android GOARCH=amd64 go build -o build/android-amd64 .
-        GOOS=android GOARCH=arm go build -o build/android-arm .
-        GOOS=android GOARCH=arm64 go build -o build/android-arm64 .
+        GOOS=linux GOARCH=amd64 go build -o build/linux-amd64 .
+        GOOS=linux GOARCH=386 go build -o build/linux-386 .
         GOOS=darwin GOARCH=386 go build -o build/darwin-386 .
         GOOS=darwin GOARCH=amd64 go build -o build/darwin-amd64 .
-        GOOS=darwin GOARCH=arm go build -o build/darwin-arm .
-        GOOS=darwin GOARCH=arm64 go build -o build/darwin-arm64 .
-        GOOS=dragonfly GOARCH=amd64 go build -o build/dragonfly-amd64 .
+        GOOS=windows GOARCH=386 go build -o build/windows-386 .
+        GOOS=windows GOARCH=amd64 go build -o build/windows-amd64 .
+        GOOS=windows GOARCH=arm go build -o build/windows-arm .
         GOOS=freebsd GOARCH=386 go build -o build/freebsd-386 .
         GOOS=freebsd GOARCH=amd64 go build -o build/freebsd-amd64 .
         GOOS=freebsd GOARCH=arm go build -o build/freebsd-arm .
         GOOS=freebsd GOARCH=arm64 go build -o build/freebsd-arm64 .
-        GOOS=js GOARCH=wasm go build -o build/js-wasm .
-        GOOS=illumos GOARCH=amd64 go build -o build/amd64 .
         GOOS=linux GOARCH=386 go build -o build/linux-386 .
         GOOS=linux GOARCH=amd64 go build -o build/linux-amd64 .
         GOOS=linux GOARCH=arm go build -o build/linux-arm .
@@ -68,24 +63,6 @@ function build-golang-app() {
         GOOS=linux GOARCH=ppc64le go build -o build/linux-ppc64le .
         GOOS=linux GOARCH=riscv64 go build -o build/linux-riscv64 .
         GOOS=linux GOARCH=s390x go build -o build/linux-s390x .
-        GOOS=nacl GOARCH=386 go build -o build/nacl-386 .
-        GOOS=nacl GOARCH=amd64p32 go build -o build/nacl-amd64p32 .
-        GOOS=nacl GOARCH=nacl-arm go build -o build/nacl-arm .
-        GOOS=netbsd GOARCH=386 go build -o build/netbsd-386 .
-        GOOS=netbsd GOARCH=amd64 go build -o build/netbsd-amd64 .
-        GOOS=netbsd GOARCH=arm go build -o build/netbsd-arm .
-        GOOS=netbsd GOARCH=arm64 go build -o build/netbsd-arm64 .
-        GOOS=openbsd GOARCH=386 go build -o build/openbsd-386 .
-        GOOS=openbsd GOARCH=amd64 go build -o build/openbsd-amd64 .
-        GOOS=openbsd GOARCH=arm go build -o build/openbsd-arm .
-        GOOS=openbsd GOARCH=arm64 go build -o build/openbsd-arm64 .
-        GOOS=plan9 GOARCH=386 go build -o build/plan9-386 .
-        GOOS=plan9 GOARCH=amd64 go build -o build/plan9-amd64 .
-        GOOS=plan9 GOARCH=arm go build -o build/plan9-arm .
-        GOOS=solaris GOARCH=amd64 go build -o build/solaris-amd64 .
-        GOOS=windows GOARCH=386 go build -o build/windows-386 .
-        GOOS=windows GOARCH=amd64 go build -o build/windows-amd64 .
-        GOOS=windows GOARCH=arm go build -o build/windows-arm .
         echo "$(find build/ -type f -print0 | xargs -0 sha1sum)" >>SHA-1
     else
         echo "Error: In your system, Go wasn't found."
