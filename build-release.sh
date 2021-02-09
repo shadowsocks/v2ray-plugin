091388e04ada6b80967b77f055d75f4edf34f753
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-09 09:00:17 -0500

    Update build-release.sh

--- build-release.sh
+++ build-release.sh
@@ -39,28 +39,32 @@ installing-system-requirements
 
 # Build for all the OS
 function build-golang-app() {
+    APPLICATION="AppName"
+    VERSION="1.0.0"
     if [ -x "$(command -v go)" ]; then
-        GOOS=darwin GOARCH=386 go build -o build/darwin-386 .
-        GOOS=darwin GOARCH=amd64 go build -o build/darwin-amd64 .
-        GOOS=windows GOARCH=386 go build -o build/windows-386.exe .
-        GOOS=windows GOARCH=amd64 go build -o build/windows-amd64.exe .
-        GOOS=windows GOARCH=arm go build -o build/windows-arm.exe .
-        GOOS=freebsd GOARCH=386 go build -o build/freebsd-386 .
-        GOOS=freebsd GOARCH=amd64 go build -o build/freebsd-amd64 .
-        GOOS=freebsd GOARCH=arm go build -o build/freebsd-arm .
-        GOOS=freebsd GOARCH=arm64 go build -o build/freebsd-arm64 .
-        GOOS=linux GOARCH=386 go build -o build/linux-386 .
-        GOOS=linux GOARCH=amd64 go build -o build/linux-amd64 .
-        GOOS=linux GOARCH=arm go build -o build/linux-arm .
-        GOOS=linux GOARCH=arm64 go build -o build/linux-arm64 .
-        GOOS=linux GOARCH=mips go build -o build/linux-mips .
-        GOOS=linux GOARCH=mips64 go build -o build/linux-mips64 .
-        GOOS=linux GOARCH=mips64le go build -o build/linux-mips64le .
-        GOOS=linux GOARCH=mipsle go build -o build/linux-mipsle .
-        GOOS=linux GOARCH=ppc64 go build -o build/linux-ppc64 .
-        GOOS=linux GOARCH=ppc64le go build -o build/linux-ppc64le .
-        GOOS=linux GOARCH=riscv64 go build -o build/linux-riscv64 .
-        GOOS=linux GOARCH=s390x go build -o build/linux-s390x .
+        GOOS=linux GOARCH=386 go build -o build/$APPLICATION-linux-386-$VERSION .
+        GOOS=linux GOARCH=amd64 go build -o build/$APPLICATION-linux-amd64-$VERSION .
+        GOOS=linux GOARCH=arm go build -o build/$APPLICATION-linux-arm-$VERSION .
+        GOOS=linux GOARCH=arm64 go build -o build/$APPLICATION-linux-arm64-$VERSION .
+        GOOS=linux GOARCH=mips go build -o build/$APPLICATION-linux-mips-$VERSION .
+        GOOS=linux GOARCH=mips64 go build -o build/$APPLICATION-linux-mips64-$VERSION .
+        GOOS=linux GOARCH=mips64le go build -o build/$APPLICATION-linux-mips64le-$VERSION .
+        GOOS=linux GOARCH=mipsle go build -o build/$APPLICATION-linux-mipsle-$VERSION .
+        GOOS=linux GOARCH=ppc64 go build -o build/$APPLICATION-linux-ppc64-$VERSION .
+        GOOS=linux GOARCH=ppc64le go build -o build/$APPLICATION-linux-ppc64le-$VERSION .
+        GOOS=linux GOARCH=riscv64 go build -o build/$APPLICATION-linux-riscv64-$VERSION .
+        GOOS=linux GOARCH=s390x go build -o build/$APPLICATION-linux-s390x-$VERSION .
+        GOOS=darwin GOARCH=386 go build -o build/$APPLICATION-darwin-386-$VERSION .
+        GOOS=darwin GOARCH=amd64 go build -o build/$APPLICATION-darwin-amd64-$VERSION .
+        GOOS=darwin GOARCH=arm go build -o build/$APPLICATION-darwin-arm-$VERSION .
+        GOOS=darwin GOARCH=arm64 go build -o build/$APPLICATION-darwin-arm64-$VERSION .
+        GOOS=freebsd GOARCH=386 go build -o build/$APPLICATION-freebsd-386-$VERSION .
+        GOOS=freebsd GOARCH=amd64 go build -o build/$APPLICATION-freebsd-amd64-$VERSION .
+        GOOS=freebsd GOARCH=arm go build -o build/$APPLICATION-freebsd-arm-$VERSION .
+        GOOS=freebsd GOARCH=arm64 go build -o build/$APPLICATION-freebsd-arm64-$VERSION .
+        GOOS=windows GOARCH=386 go build -o build/$APPLICATION-windows-386-$VERSION.exe .
+        GOOS=windows GOARCH=amd64 go build -o build/$APPLICATION-windows-amd64-$VERSION.exe .
+        GOOS=windows GOARCH=arm go build -o build/$APPLICATION-windows-arm-$VERSION.exe .
         echo "$(find build/ -type f -print0 | xargs -0 sha1sum)" >>SHA-1
     else
         echo "Error: In your system, Go wasn't found."
